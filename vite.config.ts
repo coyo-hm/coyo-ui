69a65151582bbe12364aff08a9fec80aa183f20f
COYO <dayeon.yoon@gmail.com>
2024-02-13 01:51:46 +0900

    build: vite-plugin-dts 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import dts from "vite-plugin-dts"
+import tsconfigPaths from "vite-tsconfig-paths"
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +10,39 @@ export default defineConfig({
 		react({
 			jsxImportSource: "@emotion/react",
 			babel: {
-				plugins: ["@emotion/babel-plugin"],
-			},
+				plugins: ["@emotion/babel-plugin"]
+			}
 		}),
+		dts({
+			// rollupTypes: true,
+			insertTypesEntry: true
+		}),
+		tsconfigPaths()
 	],
-});
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.ts'),
+			name: 'coyo-ui',
+			formats: ['es', 'cjs'],
+			fileName: (format) => `index.${format}.js`,
+		},
+		rollupOptions: {
+			external: [
+				'react',
+				'react-dom',
+				'styled-components',
+			],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+				banner: '"use client";',
+				interop: 'compat',
+			},
+		},
+		commonjsOptions: {
+			esmExternals: ["react"],
+		},
+	}
+})
