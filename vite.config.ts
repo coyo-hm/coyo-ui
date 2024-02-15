46b0947cdef0a8d11e1411db53380f7d04db5738
COYO <dayeon.yoon@gmail.com>
2024-02-16 03:19:32 +0900

    chore: vite config 오타 수정

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
 			fileName: format => `index.${format}.js`
 		},
 		rollupOptions: {
-			external: ["react", "react-dom", "styled-components"],
+			external: ["react", "react-dom"],
 			output: {
 				globals: {
 					react: "React",
