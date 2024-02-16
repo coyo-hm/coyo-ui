bc059e6ace090eead8b3b1a87abbd3f87647916f
COYO <dayeon.yoon@gmail.com>
2024-02-17 02:48:18 +0900

    fix: testcomponent

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,7 +1,8 @@
 import { TestComponentType } from "./TestComponent.type.ts"
+import { css } from "@emotion/react"
 
 const TestComponent = ({ ...props }: TestComponentType) => {
-	return <div {...props} >TEST</div>
+	return <div {...props} css={css`background-color: green`} >TEST</div>
 }
 
 export default TestComponent
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
 'use client'
 
-export * from './components'
+// export * from './components'
+export { default as Marquee } from "./components/Marquee/Marquee.tsx";
+export { default as TestComponent } from "./components/TestComponent/TestComponent.tsx";
